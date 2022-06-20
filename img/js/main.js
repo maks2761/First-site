00a7a938179b4f3132adb64d71f1c7c6673e3ca4
maks2761 <50800476+maks2761@users.noreply.github.com>
2022-06-20 21:53:46 +0800

    Add files via upload

---
+++ img/js/main.js
@@ -0,0 +1,9 @@
+const navBtn = document.querySelector('.nav-button')
+const mobileNav = document.querySelector('.mobile-nav')
+
+navBtn.addEventListener('click', function () {
+    mobileNav.classList.toggle('mobile-nav-active')
+    navBtn.classList.toggle('nav-button-close')
+
+
+})
